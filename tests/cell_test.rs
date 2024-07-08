dbc39d4e9d9c2cbab5992f68cdfd9ceeaa26f240
ItsukiMakino <107731182+ItsukiMakino@users.noreply.github.com>
2024-07-08 23:40:09 +0900

    fix test

--- tests/cell_test.rs
+++ tests/cell_test.rs
@@ -1,28 +1,15 @@
-extern crate mineswepper;
-use bevy::prelude::*;
+use bevy::{input::InputPlugin, prelude::*, };
 use mineswepper::cell::{self, component::CellButton};
 use once_cell::sync::Lazy;
 use std::sync::Once;
 
-static INIT: Lazy<Once> = Lazy::new(|| Once::new());
-
-fn setup() {
-    todo!();
-}
-
-fn initialize() {
-    INIT.call_once(|| {
-        setup();
-    });
-}
 #[test]
 fn grid_spawn_test() {
-    initialize();
     let mut app = App::new();
     app.add_plugins(MinimalPlugins);
     app.add_plugins(cell::MyPlugin);
+    app.add_plugins(InputPlugin);
     app.update();
-
     assert_eq!(
         app.world_mut()
             .query::<&CellButton>()
@@ -33,6 +20,5 @@ fn grid_spawn_test() {
 }
 #[test]
 fn test_example() {
-    initialize();
     assert_ne!(1 + 1, 1);
 }
