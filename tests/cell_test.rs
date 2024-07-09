cca143fc6894215610ff86beb973fba6b21121a2
ItsukiMakino <107731182+ItsukiMakino@users.noreply.github.com>
2024-07-09 16:37:09 +0900

    add get_neighbor_cell_test

--- tests/cell_test.rs
+++ tests/cell_test.rs
@@ -21,3 +21,33 @@ fn grid_spawn_test() {
 fn test_example() {
     assert_ne!(1 + 1, 1);
 }
+#[test]
+fn get_neighboring_indices()
+{
+    let grid_size = 256;
+    let index = 0;
+    let row_size = (grid_size as f64).sqrt() as u8;
+    let row = index / row_size;
+    let col = index % row_size;
+
+    let mut neighbors = Vec::new();
+
+    for y in -1..=1 {
+        for x in -1..=1 {
+            if y == 0 && x == 0 {
+                continue;
+            }
+            let new_row = row as isize + y;
+            let new_col = col as isize + x;
+
+            if new_row >= 0 && new_row < row_size as isize && new_col >= 0 && new_col < row_size as isize {
+                neighbors.push((new_row as u8 * row_size) + new_col as u8);
+            }
+        }
+    }
+    assert_eq!(neighbors.len(),3);
+    assert_eq!(neighbors[0], 1);
+    assert_eq!(neighbors[1], 16);
+    assert_eq!(neighbors[2], 17);
+
+}
